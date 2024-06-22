ae5814c14736467a4f1b374d08e3e9a218f148db
Devansh Kukreja <devansh.mmis@gmail.com>
2024-06-22 14:33:15 +0530

    basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { index } from "drizzle-orm/mysql-core";
 import Link from "next/link";
+
+const mockUrls = [
+  "https://utfs.io/f/df253b36-f5b3-47c7-90b2-089b456baa56-ocy5td.jpeg",
+  "https://utfs.io/f/29e1499c-fe80-4661-997a-e56a759c0dd4-daa6ru.png",
+  "https://utfs.io/f/fe89e7db-5d5c-45b8-a65a-f9f6a084b8e6-dspw9u.png",
+  "https://utfs.io/f/dc682ca7-181f-48ba-9d75-040ea4ca3cbd-r05z7r.jpeg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello there (Gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
